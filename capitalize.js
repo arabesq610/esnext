dd7c2090b5cd2972fba10655ae60d0ced0f6c9ce
Elise Linn <(none)>
2017-07-11 16:18:50 -0700

    with arrow functions

---
+++ capitalize.js
@@ -0,0 +1,14 @@
+const capitalize = x =>
+    x[0].toUpperCase() + x.substr(1)
+
+const doubleSay = x => x + ', ' + x
+const exclaim = x => x + '!'
+
+// const x = 'hello' > capitalize
+
+// let y = x
+
+//console.log(x);
+
+const x = exclaim('hello')
+//x hello!
